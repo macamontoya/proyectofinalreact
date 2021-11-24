054e99f935c6f142b4e02ef92a6fc39bdf80bbbc
macamontoya <56725506+macamontoya@users.noreply.github.com>
2021-11-24 13:08:22 -0300

    comentarios

--- src/screens/newpost.js
+++ src/screens/newpost.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
-import Camara from '../components/camara'
-import { auth, db } from '../firebase/config'
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native' //componentes core (predefinidos de react)
+import Camara from '../components/camara' //usamos este componente para crear un posteo 
+import { auth, db } from '../firebase/config' 
 
 export default class newpost extends Component {
     constructor(){
@@ -12,7 +12,7 @@ export default class newpost extends Component {
             textoPost: ''
         }
     }
-    onImageUpload(url){
+    onImageUpload(url){ //le entra a una url que es el link de la imagen, yo tengo el url porque ya saq la foto, voy a gaurdar dentro de la url la informacion de img.
         this.setState({
             mostrarcamara: false,
             url: url
@@ -21,14 +21,14 @@ export default class newpost extends Component {
     submitPost(){
 
         db.collection('posts').add({ // a la collection de post agregale este post, adentro tiene los datos que le voy a guardar al post
-            owner: auth.currentUser.email, //usuario logeado, quiero el email
-            texto: this.state.textoPost, // descripcion del text
-            createdAt: Date.now(),
-            photo: this.state.url, //link d foto
+            owner: auth.currentUser.email, 
+            texto: this.state.textoPost, 
+            createdAt: Date.now(), //js
+            photo: this.state.url, // me guarda en la base de datos la url q es el link d la imagen
             likes: [],
             comentarios:[],
         })
-        .then( ()=>{ //Limpiar el form de carga
+        .then( ()=>{ //puedo crear otro post
             this.setState({
                 textoPost:'',
                 mostrarcamara: true,
@@ -41,10 +41,10 @@ export default class newpost extends Component {
         return (
             <View style= {styles.container}>
                 {this.state.mostrarcamara ? 
-                <Camara onImageUpload={(url) => this.onImageUpload(url)}/>: 
+                <Camara onImageUpload={(url) => this.onImageUpload(url)}/>:
                 <View >
                 <TextInput
-                    onChangeText={(text)=>this.setState({textoPost: text})}
+                    onChangeText={(text)=>this.setState({textoPost: text})} //guardo dentro de texto post la informacion que se escribe
                     placeholder='Escribí aquí'
                     keyboardType='default'
                     multiline
